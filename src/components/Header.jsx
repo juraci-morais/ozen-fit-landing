a94c73cd951e60968e40930c84b45f5bcba7e2f2
juraci-morais <adrijuvibesync@gmail.com>
2026-03-13 10:25:07 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header style={{background:"#fff", color:"#fff"}}>
+      <img
+src="/logo1.png"
+alt="logo"
+style={{width:"380px",margin:"20px auto"}}
+/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/RatingBar.jsx
@@ -0,0 +1,19 @@
+export default function RatingBar() {
+
+return (
+
+<div style={{
+  background:"#16a34a",
+  color:"#fff",
+  textAlign:"center",
+  padding:"8px",
+  fontWeight:"bold"
+}}>
+
+  ⭐⭐⭐⭐⭐ 4.8/5 | Mais de 12.784 clientes satisfeitos
+
+</div>
+
+);
+
+}
